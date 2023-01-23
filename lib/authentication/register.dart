7654f7f49a02c611140622c35abe5867b9e8abd3
Bera <choibera1389@gmail.com>
2023-01-23 17:08:20 -0600

    18. Checked if seller Already Logged-in or NOT

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:seller_app/mainScreens/home_screen.dart';
 import 'package:seller_app/widgets/custom_text_field.dart';
 import 'package:seller_app/widgets/error_dialog.dart';
 import 'package:seller_app/widgets/loading_dialog.dart';
 import 'package:firebase_storage/firebase_storage.dart' as fStorage;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -33,6 +37,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   String sellerImageUrl = "";
 
+  //16강-------------------------------------------------------------------------
+  String completeAddress = "";
+
   Future<void> _getImage() async{
     imageXFile = await _picker.pickImage(source: ImageSource.gallery);
 
@@ -49,7 +56,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
       position!.longitude,
     );
     Placemark pMark = placemarks![0];
-    String completeAddress = '${pMark.subThoroughfare} ${pMark.thoroughfare}, ${pMark.subLocality} ${pMark.locality}, ${pMark.subAdministrativeArea} ${pMark.administrativeArea} ${pMark.postalCode}, ${pMark.country}';
+
+// 16강----------------------------------------------------------------------------
+//    String completeAddress = '${pMark.subThoroughfare} ${pMark.thoroughfare}, ${pMark.subLocality} ${pMark.locality}, ${pMark.subAdministrativeArea} ${pMark.administrativeArea} ${pMark.postalCode}, ${pMark.country}';
+    completeAddress = '${pMark.subThoroughfare} ${pMark.thoroughfare}, ${pMark.subLocality} ${pMark.locality}, ${pMark.subAdministrativeArea} ${pMark.administrativeArea} ${pMark.postalCode}, ${pMark.country}';
     locationController.text = completeAddress;
   }
 
@@ -83,6 +93,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
           await taskSnapshot.ref.getDownloadURL().then((url) {
             sellerImageUrl = url;
             // save info to firestore
+            authenticateSellerAndSignUp();
           });
         }
         else{
@@ -109,110 +120,174 @@ class _RegisterScreenState extends State<RegisterScreen> {
     }
   }
 
+// 16강--------------------------------------------------------------------------
+
+  void authenticateSellerAndSignUp() async {
+    User? currentUser;
+    final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    await firebaseAuth.createUserWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    ).then((auth) {
+      currentUser = auth.user;
+
+    //todo 17강 시작
+    }).catchError((error){
+      Navigator.pop(context);
+      showDialog(
+        context: context,
+        builder: (c){
+          return ErrorDialog(
+            message: error.message.toString(),
+          );
+        }
+      );
+      //todo 17강 끝
+    });
+
+    if(currentUser != null){
+      saveDataToFirestore(currentUser!).then((value){
+        Navigator.pop(context);
+        //send user to homePage
+        Route newRoute = MaterialPageRoute(builder: (c) => HomeScreen());
+        Navigator.pushReplacement(context, newRoute);
+      });
+    }
+  }
+
+  Future saveDataToFirestore(User currentUser) async{
+    FirebaseFirestore.instance.collection("sellers").doc(currentUser.uid).set({
+      "sellerUID" : currentUser.uid,
+      "sellerEmail" : currentUser.email,
+      "sellerName" : nameController.text.trim(),
+      "sellerAvatarUrl" : sellerImageUrl,
+      "phone" : phoneController.text.trim(),
+      "address" : completeAddress,
+      "status" : "approved",
+      "earnings" : 0.0,
+      "lat" : position!.latitude,
+      "lng" : position!.longitude,
+    });
+
+//todo 17강 시작
+
+    SharedPreferences? sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setString("uid", currentUser.uid);
+    await sharedPreferences.setString("email", currentUser.email.toString());
+    await sharedPreferences.setString("name", nameController.text.trim());
+    await sharedPreferences.setString("photoUrl", sellerImageUrl);
+  }
+
+//todo 17강 끝
+
+// 16강--------------------------------------------------------------------------
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          const SizedBox(height: 10,),
-          InkWell(
-            onTap: (){
-              _getImage();
-            },
-            child: CircleAvatar(
-              radius: MediaQuery.of(context).size.width * 0.20,
-              backgroundColor: Colors.white,
-              backgroundImage: imageXFile==null ? null : FileImage(File(imageXFile!.path)),
-              child: imageXFile == null ? Icon(
-                Icons.add_photo_alternate,
-                size: MediaQuery.of(context).size.width * 0.20,
-                color: Colors.grey,
-                ) : null,
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const SizedBox(height: 10,),
+            InkWell(
+              onTap: (){
+                _getImage();
+              },
+              child: CircleAvatar(
+                radius: MediaQuery.of(context).size.width * 0.20,
+                backgroundColor: Colors.white,
+                backgroundImage: imageXFile==null ? null : FileImage(File(imageXFile!.path)),
+                child: imageXFile == null ? Icon(
+                  Icons.add_photo_alternate,
+                  size: MediaQuery.of(context).size.width * 0.20,
+                  color: Colors.grey,
+                  ) : null,
+              ),
             ),
-          ),
-          const SizedBox(height: 10,),
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                CustomTextField(
-                  data: Icons.person,
-                  controller: nameController,
-                  hintText: "Name",
-                  isObsecre: false,
-                ),
-                CustomTextField(
-                  data: Icons.email,
-                  controller: emailController,
-                  hintText: "Email",
-                  isObsecre: false,
-                ),
-                CustomTextField(
-                  data: Icons.lock,
-                  controller: passwordController,
-                  hintText: "Password",
-                  isObsecre: true,
-                ),
-                CustomTextField(
-                  data: Icons.lock,
-                  controller: confirmPasswordController,
-                  hintText: "Confirm Password",
-                  isObsecre: true,
-                ),
-                CustomTextField(
-                  data: Icons.phone,
-                  controller: phoneController,
-                  hintText: "Phone",
-                  isObsecre: false,
-                ),
-                CustomTextField(
-                  data: Icons.my_location,
-                  controller: locationController,
-                  hintText: "Cafe/Restaurent Address",
-                  isObsecre: false,
-                  enabled: false,
-                ),
-                Container(
-                  width: 400,
-                  height: 40,
-                  alignment: Alignment.center,
-                  child: ElevatedButton.icon(
-                    label: const Text(
-                      "Get my Current Location",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () {
-                      getCurrentLocation();
-                    },
-                    icon: const Icon(Icons.location_on, color: Colors.white,),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.amber,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      )
-                    ),
+            const SizedBox(height: 10,),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    data: Icons.person,
+                    controller: nameController,
+                    hintText: "Name",
+                    isObsecre: false,
                   ),
-                )
-              ],
+                  CustomTextField(
+                    data: Icons.email,
+                    controller: emailController,
+                    hintText: "Email",
+                    isObsecre: false,
+                  ),
+                  CustomTextField(
+                    data: Icons.lock,
+                    controller: passwordController,
+                    hintText: "Password",
+                    isObsecre: true,
+                  ),
+                  CustomTextField(
+                    data: Icons.lock,
+                    controller: confirmPasswordController,
+                    hintText: "Confirm Password",
+                    isObsecre: true,
+                  ),
+                  CustomTextField(
+                    data: Icons.phone,
+                    controller: phoneController,
+                    hintText: "Phone",
+                    isObsecre: false,
+                  ),
+                  CustomTextField(
+                    data: Icons.my_location,
+                    controller: locationController,
+                    hintText: "Cafe/Restaurent Address",
+                    isObsecre: false,
+                    enabled: false,
+                  ),
+                  Container(
+                    width: 400,
+                    height: 40,
+                    alignment: Alignment.center,
+                    child: ElevatedButton.icon(
+                      label: const Text(
+                        "Get my Current Location",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        getCurrentLocation();
+                      },
+                      icon: const Icon(Icons.location_on, color: Colors.white,),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.amber,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        )
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: 30,),
-          ElevatedButton(
-            child: const Text(
-              "Sign Up",
-              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            const SizedBox(height: 30,),
+            ElevatedButton(
+              child: const Text(
+                "Sign Up",
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.purple,
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 20)
+              ),
+              onPressed: (){
+                formValidation();
+              },
             ),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.purple,
-              padding: EdgeInsets.symmetric(horizontal: 80, vertical: 20)
-            ),
-            onPressed: (){
-              formValidation();
-            },
-          ),
-          const SizedBox(height: 30,),
-      ],
+            const SizedBox(height: 30,),
+        ],
+        ),
       ),
     );
   }
