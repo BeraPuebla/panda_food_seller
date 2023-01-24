1d519dc6ef02c96c4cc16c99889004b87f95ec52
Bera <choibera1389@gmail.com>
2023-01-23 22:42:05 -0600

    20. Allow seller to Login and save Data Locally using SharedPrefrences

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:seller_app/global/global.dart';
+import 'package:seller_app/mainScreens/home_screen.dart';
 import 'package:seller_app/widgets/custom_text_field.dart';
+import 'package:seller_app/widgets/error_dialog.dart';
+import 'package:seller_app/widgets/loading_dialog.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,6 +19,52 @@ class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  formValidation(){
+    if(emailController.text.isNotEmpty && passwordController.text.isNotEmpty){
+      //login
+      loginNow();
+    }
+    else{
+      showDialog(context: context, builder: (c){
+        return const ErrorDialog(message: "Please wirte email/password.",);
+      });
+    }
+  }
+
+  loginNow() async {
+    showDialog(context: context, builder: (c){
+        return const LoadingDialog(message: "Checking Credentials",);
+      });
+
+      User? currentUser;
+      await firebaseAuth.signInWithEmailAndPassword(email: emailController.text.trim(), password: passwordController.text.trim()
+      ).then((auth) {
+        currentUser = auth.user!;
+      }).catchError((error){
+        Navigator.pop(context);
+        showDialog(context: context, builder: (c){
+        return ErrorDialog(message: error.message.toString(),);
+      });
+      });
+      if(currentUser != null){
+        readDataAndSetDataLocally(currentUser!).then((value){
+          Navigator.pop(context);
+          Navigator.push(context, MaterialPageRoute(builder: (c)=>const HomeScreen()));
+        });
+      }
+  }
+
+  Future readDataAndSetDataLocally(User currentUser) async {
+    await FirebaseFirestore.instance.collection("sellers")
+    .doc(currentUser.uid)
+    .get().then((snapshot) async {
+      await sharedPreferences!.setString("uid", currentUser.uid);
+      await sharedPreferences!.setString("email", snapshot.data()!["sellerEmail"]);
+      await sharedPreferences!.setString("name", snapshot.data()!["sellerName"]);
+      await sharedPreferences!.setString("photoUrl", snapshot.data()!["sellerAvatarUrl"]);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +104,9 @@ class _LoginScreenState extends State<LoginScreen> {
               primary: Colors.cyan,
               padding: EdgeInsets.symmetric(horizontal: 80, vertical: 20)
             ),
-            onPressed: () => print("clicked"),
+            onPressed: () {
+              formValidation();
+            },
           ),
           const SizedBox(height: 30,),
         ],
