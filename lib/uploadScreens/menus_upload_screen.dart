e968d66250861061da8a23d2b85dd3408bae4efe
Bera <choibera1389@gmail.com>
2023-02-07 14:33:20 -0600

    45. Implement Upload Menus Form Screen Design

--- lib/uploadScreens/menus_upload_screen.dart
+++ lib/uploadScreens/menus_upload_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:seller_app/mainScreens/home_screen.dart';
@@ -12,6 +14,9 @@ class MenusUploadScreen extends StatefulWidget {
 class _MenusUploadScreenState extends State<MenusUploadScreen> {
   XFile? imageXFile;
   final ImagePicker _picker = ImagePicker();
+
+  TextEditingController shortInfoController = TextEditingController();
+  TextEditingController titleController = TextEditingController();
 
   defaultScreen() {
     return Scaffold(
@@ -123,8 +128,80 @@ class _MenusUploadScreenState extends State<MenusUploadScreen> {
     });
   }
 
+  menusUploadFormScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.cyan, Colors.amber],
+                  begin: FractionalOffset(0.0, 0.0),
+                  end: FractionalOffset(1.0, 0.0),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp)),
+        ),
+        title: const Text(
+          "Uploading New Menu",
+          style: TextStyle(fontSize: 20, fontFamily: "Lobster"),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            //Navigator.push(
+            //   context, MaterialPageRoute(builder: (c) => const HomeScreen()));
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              "Add",
+              style: TextStyle(fontSize: 20),
+            ),
+          )
+        ],
+      ),
+      body: ListView(children: [
+        SizedBox(
+          height: 230,
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: Center(
+            child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: FileImage(File(imageXFile!.path)),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            ),
+          ),
+        ),
+        ListTile(
+          leading: const Icon(
+            Icons.perm_device_information,
+            color: Colors.cyan,
+          ),
+          title: SizedBox(
+            width: 250,
+            child: TextField(
+              controller: shortInfoController,
+              decoration: const InputDecoration(hintText: "Menu info"),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return defaultScreen();
+    return imageXFile == null ? defaultScreen() : menusUploadFormScreen();
   }
 }
